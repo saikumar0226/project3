26af43ce6ae06003f5bb792e3568684d4faa2eee
saikumar0226 <pakkurthisaikumar97@gmail.com>
2020-07-20 23:23:27 +0530

    Added

---
+++ src/com/company/pattern2.java
@@ -0,0 +1,22 @@
+/*
+
+  7 6 5 4 3 2 1
+
+ */
+
+package com.company;
+
+import java.util.Scanner;
+
+public class pattern2 {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i =n ; i>=1; i--)
+        {
+            System.out.print(i + " ");
+        }
+
+    }
+}
---
+++ src/pattern4.java
@@ -0,0 +1,23 @@
+/*
+ * * * * *
+ * * * *
+ * * *
+ * *
+ *
+
+ */
+import java.util.Scanner;
+
+public class pattern4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your number: ");
+        int n = sc.nextInt();
+        for (int i = n; i >= 0; i--) {
+            for (int j = i; j >= 0; j--)
+                System.out.print("* ");
+
+            System.out.println();
+        }
+    }
+}
